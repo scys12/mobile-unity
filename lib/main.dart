bdf3df7232c7e6c558b3652c0d3fd2b6d2351538
HellBus1 <syubbanfakhriya@gmail.com>
2021-04-14 16:44:13 +0700

    init children dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_unity/src/models/user.dart';
 import 'package:mobile_unity/src/pages/auth/authenticate.dart';
+import 'package:mobile_unity/src/pages/child/dashboard.dart';
 import 'package:mobile_unity/src/pages/parent/add_child.dart';
 import 'package:mobile_unity/src/pages/parent/child_task.dart';
 import 'package:mobile_unity/src/pages/parent/dashboard.dart';
@@ -26,30 +27,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: _fbApp,
-      builder: (context, snapshot){
-        if(snapshot.hasError) {
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
           print('You have an error! ${snapshot.error.toString()}');
           return Text('Something went wrong');
-        }else if(snapshot.hasData) {
+        } else if (snapshot.hasData) {
           return StreamProvider<User>.value(
             value: AuthService().user,
             initialData: User(name: '', uid: ''),
             child: MaterialApp(
               home: Wrapper(),
               routes: {
-                '/authenticate' : (context) => Authenticate(),
-                '/parent/dashboard' : (context) => DashboardParent(),
-                '/parent/task' : (context) => ChildTask(),
-                '/parent/add_child' : (context) => AddChildScreen(),
-                '/parent/new_task' : (context) => NewTaskChild(),
-                '/parent/new_education' : (context) => NewEducationChild(),
-                '/parent/all_tasks' : (context) => ListChildTasks(),
-                '/parent/all_educations' : (context) => ListChildEducations(),
-                '/parent/detail_task' : (context) => DetailTaskChild()
+                '/authenticate': (context) => Authenticate(),
+                '/parent/dashboard': (context) => DashboardParent(),
+                '/parent/task': (context) => ChildTask(),
+                '/parent/add_child': (context) => AddChildScreen(),
+                '/parent/new_task': (context) => NewTaskChild(),
+                '/parent/new_education': (context) => NewEducationChild(),
+                '/parent/all_tasks': (context) => ListChildTasks(),
+                '/parent/all_educations': (context) => ListChildEducations(),
+                '/parent/detail_task': (context) => DetailTaskChild(),
+                '/child/dashboard': (context) => DashboardChild()
               },
             ),
           );
-        }else {
+        } else {
           return Center(
             child: CircularProgressIndicator(),
           );
---
+++ lib/src/pages/child/dashboard.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DashboardChild extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _State();
+  }
+}
+
+class _State extends State<DashboardChild> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
